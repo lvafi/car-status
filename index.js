cce954cb6b9aa51448904a02247917263bf6427f
ladan-vafi <lydia.vafi@gmail.com>
2020-12-14 19:42:01 -0800

    Express web app on local host

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+// the only thing we need to do to get express up and running is to import it using requiew  
 const express = require('express');
+// this express that has imported is acting like a function so we call it and return value of that function is an app object 
 const app = express();
+// app is an object that has methods you use to build a web server
 
 //app is an object that has methods you use to build a web server
 app.get("/hello_world", (request, response) => {
     response.send("Hello World!");
 });
 
-
+const ADDRESS = 'localhost'
+const PORT = 3000;
 app.listen(3000, () => {
-    console.log('Express web app on localhost: 3000');
+    console.log(`Server listening on ${ADDRESS}: ${PORT}`);
 });

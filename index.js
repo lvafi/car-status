19a2bd1b070574f7f412194212c36830eb237cc9
ladan-vafi <lydia.vafi@gmail.com>
2020-12-14 20:53:15 -0800

    Make Route for our app

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // the only thing we need to do to get express up and running is to import it using requiew  
 const express = require('express');
+const { response } = require('express');
 // this express that has imported is acting like a function so we call it and return value of that function is an app object 
 const app = express();
 // app is an object that has methods you use to build a web server
@@ -9,8 +10,13 @@ app.get("/hello_world", (request, response) => {
     response.send("Hello World!");
 });
 
-const ADDRESS = 'localhost'
+// route is basicly a function that will handle some sort of request coming into our applecation
+app.get('/hello_world', (request, response) => {
+    response.send('Hello World');
+})
+
+const DOMAIN = 'localhost'
 const PORT = 3000;
 app.listen(3000, () => {
-    console.log(`Server listening on ${ADDRESS}: ${PORT}`);
+    console.log(`The server is listening at ${DOMAIN}:${PORT}`);
 });

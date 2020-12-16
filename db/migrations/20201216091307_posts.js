4198babeb30e49d00e1bec13a93006b62f5cf2b9
ladan-vafi <lydia.vafi@gmail.com>
2020-12-16 09:23:41 -0800

    modify Migrations & client.js file

--- db/migrations/20201216091307_posts.js
+++ db/migrations/20201216091307_posts.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable('posts', table => {
+      table.increments('id');
+      table.string('title');
+      table.text('content');
+      table.string('image_url');
+      table.integer('view_count');
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+    })
+  };
   
-};
-
-exports.down = function(knex) {
-  
-};
+  exports.down = function(knex) {
+    return knex.schema.dropTable('posts');
+  };
